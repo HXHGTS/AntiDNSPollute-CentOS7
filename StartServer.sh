7ed6d83c7744d38653c4f2d62c8076fbdf0de3f3
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-12 12:13:13 +0800

    Update StartServer.sh

--- StartServer.sh
+++ StartServer.sh
@@ -2,7 +2,7 @@
 
 echo "正在搭建无污染DNS. . . . . ."
 
-yum install -y dnsmasq curl iptables
+yum install -y dnsmasq curl iptables-services
 
 systemctl enable iptables
 
@@ -10,7 +10,11 @@ systemctl start iptables
 
 echo "no-resolv"> /etc/dnsmasq.conf
 
-echo "listen-address=0.0.0.0,127.0.0.1">> /etc/dnsmasq.conf
+ip=echo $(ip addr |grep inet |grep -v inet6 |grep eth0|awk '{print $2}' |awk -F "/" '{print $1}')
+
+echo listen-address=$ip,127.0.0.1>> /etc/dnsmasq.conf
+
+echo "port=5353">> /etc/dnsmasq.conf
 
 echo "server=208.67.222.222#5353">> /etc/dnsmasq.conf
 
@@ -20,11 +24,11 @@ echo "server=208.67.222.220#5353">> /etc/dnsmasq.conf
 
 echo "server=208.67.220.220#5353">> /etc/dnsmasq.conf
 
-curl -s https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf|sed 's/119.29.29.29/g' >/etc/dnsmasq.d/accelerated-domains.china.conf
+curl -s https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf|sed 's/119.29.29.29/182.254.116.116/g' >/etc/dnsmasq.d/accelerated-domains.china.conf
 
-iptables -t nat -I PREROUTING -p tcp --dport 5353 -j REDIRECT --to-ports 53
+iptables -t nat -I PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 5353
 
-iptables -t nat -I PREROUTING -p udp --dport 5353 -j REDIRECT --to-ports 53
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5353
 
 systemctl enable dnsmasq
 
@@ -34,7 +38,7 @@ echo "nameserver 127.0.0.1"> /etc/resolv.conf.conf
 
 echo "无污染DNS搭建完成!"
 
-echo "对外开放端口默认53与5353"
+echo "对外开放端口默认5353"
 
 exit
 
