202279f95a7af0a779c92f6c1fcc250937af54a5
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-12 11:46:13 +0800

    Update StartServer.sh

--- StartServer.sh
+++ StartServer.sh
@@ -8,6 +8,10 @@ echo "listen-address=0.0.0.0,127.0.0.1"> /etc/dnsmasq.conf
 
 echo "server=208.67.222.222#5353">> /etc/dnsmasq.conf
 
+iptables -t nat -I PREROUTING -p tcp --dport 5353 -j REDIRECT --to-ports 53
+
+iptables -t nat -I PREROUTING -p udp --dport 5353 -j REDIRECT --to-ports 53
+
 systemctl enable dnsmasq
 
 systemctl start dnsmasq
@@ -15,6 +19,8 @@ systemctl start dnsmasq
 echo "nameserver 127.0.0.1">> /etc/resolv.conf.conf
 
 echo "无污染DNS搭建完成!"
+
+echo "对外开放端口默认53与5353"
 
 exit
 
