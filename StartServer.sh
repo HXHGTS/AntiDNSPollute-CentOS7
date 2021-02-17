c6fe371991cd599505b2c53893a19099f09f05ee
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-02-17 20:42:04 +0800

    Update StartServer.sh

--- StartServer.sh
+++ StartServer.sh
@@ -12,11 +12,19 @@ echo "resolv-file=/etc/resolv.dnsmasq.conf" >> /etc/dnsmasq.conf
 
 echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
 
+echo "server=208.67.222.222#5353" >> /etc/dnsmasq.conf
+
+echo "server=208.67.220.222#5353" >> /etc/dnsmasq.conf
+
+echo "server=208.67.222.220#5353" >> /etc/dnsmasq.conf
+
+echo "server=208.67.220.220#5353" >> /etc/dnsmasq.conf
+
 echo "port=9953">> /etc/dnsmasq.conf
 
-echo "nameserver 208.67.222.222"> /etc/resolv.dnsmasq.conf
+echo "nameserver 100.100.2.136"> /etc/resolv.dnsmasq.conf
 
-echo "nameserver 208.67.220.220">> /etc/resolv.dnsmasq.conf
+echo "nameserver 100.100.2.138">> /etc/resolv.dnsmasq.conf
 
 mkdir /etc/dnsmasq.d
 
